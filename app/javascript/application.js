74688a2a3ddf2e7dc86c625b2919376a5e7e544a
zain jafar <zain.jaffar@algorepublic.com>
2025-07-04 14:09:39 +0500

    refactor code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,28 +6,27 @@ import $ from "jquery"
 window.$ = $
 window.jQuery = $
 
-import './styles/application-v2.css'
+import './styles/application-v2.css';
 // node packages(features)
-import "jquery-bar-rating/dist/jquery.barrating.min.js"
+import "jquery-bar-rating/dist/jquery.barrating.min.js";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "chart.js/dist/Chart.min.js"
-import psLib from "perfect-scrollbar";
-window.PerfectScrollbar = psLib?.default || psLib;
+import "chart.js/dist/Chart.min.js";
 document.addEventListener("turbo:load", async () => {
     await import("jquery-sparkline");
-    const raphael = await import("./vendor/raphael");
+    const raphael = await import("./vendor/raphael.min.js");
     window.Raphael = raphael.default || raphael;
-    await import("./vendor/morris.js");
+    await import("./vendor/morris.min.js");
 });
 // custom js files
-import "./js/off-canvas.js"
-import "./js/hoverable-collapse.js"
-import "./js/misc.js"
-import "./js/settings.js"
-import "./js/todolist.js"
-import "./js/dashboard.js"
+import "./js/off-canvas.js";
+import "./js/hoverable-collapse.js";
+import "./js/misc.js";
+import "./js/settings.js";
+import "./js/todolist.js";
+import "./js/dashboard.js";
 
-
+// import psLib from "perfect-scrollbar";
+// window.PerfectScrollbar = psLib?.default || psLib;
 // document.addEventListener("turbo:load", () => {
 //     const el = document.querySelector("#scroll-container");
 //     if (el && window.PerfectScrollbar) {
